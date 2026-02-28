b04a26dba7bb1469cc05c8a278d4e31acef48222
Raylee <abramscarter1999@gmail.com>
2026-02-27 21:00:01 -0500

    update encoding alg functions and move it to a class

--- encoding alg.py
+++ encoding alg.py
@@ -1,68 +1,82 @@
-# exmaple/test input
-binput = "1101100001101100111010000110101010111010000101101010000001100100"
-print(binput)
-# split up long numbers
-def longNumSplitter(count):
-    longstr=""
-    for char in str(count):
-        longstr+=char
-        count=str(count)[1:]
-        if len(count)>0:
-            longstr+="/"
-    return longstr
-def updateNum(curNum):
-    if curNum==1:
-        return 0
-    elif curNum==0:
-        return 1
-# encode input
-def encode(binaryinput):
-    numcounter=0
-    final=binaryinput[0]
-    for i in range(len(binaryinput)):
-        if binaryinput[i]==binaryinput[i-1]:
-            numcounter+=1
-        else: 
-            if numcounter<9:
-                final+=str(numcounter)
-                numcounter=1
+class Encoder:
+    # split up long numbers
+    @staticmethod
+    def longNumSplitter(count):
+        longstr=""
+
+        for char in str(count):
+            longstr+=char
+            count=str(count)[1:]
+
+            if len(count)>0:
+                longstr+="/"
+
+        return longstr
+    
+    @staticmethod
+    def updateNum(curNum):
+        if curNum==1:
+            return 0
+        elif curNum==0:
+            return 1
+    
+    # encode input
+    @staticmethod
+    def encode(binaryinput):
+        numcounter=1
+        final=binaryinput[0]
+
+        for i in range(1,len(binaryinput)):
+
+            if binaryinput[i]==binaryinput[i-1]:
+                numcounter+=1
             else: 
-                final+=longNumSplitter(numcounter)
-                numcounter=1
-    final+=longNumSplitter(numcounter)
-    numcounter=1
-    # clean and return output
-    if final[0]=="0":
+                if numcounter<9:
+                    final+=str(numcounter)
+                    numcounter=1
+                else: 
+                    final+=Encoder.longNumSplitter(numcounter)
+                    numcounter=1
+        
+        final+=Encoder.longNumSplitter(numcounter)
+        numcounter=1
+
+        # clean and return output
+        if final[0]=="0":
+            final=final[1:]
+        return final
+
+    # take in input and decode it 
+    @staticmethod
+    def decode(binaryinput):
+        final=""
+        currentNum=int(binaryinput[0])
+
+        for i in range(1,len(binaryinput)):
+            # need to also check if next char is / without overflow
+            if not (i+1)==len(binaryinput):
+                if binaryinput[i]!="/" and binaryinput[i-1]!="/" and binaryinput[i+1]!="/":
+                    # add that amount of the currentNum to final
+                    for n in range(int(binaryinput[i])):
+                        final+=str(currentNum)
+                elif binaryinput[i]=="/":
+                    # parse longer numbers
+                    amount=binaryinput[i-1]+binaryinput[i+1]
+
+                    for j in range(int(amount)):
+                        final+=str(currentNum)
+                else:
+                    continue
+            
+            # update current number
+            currentNum=Encoder.updateNum(currentNum)
+        
+        # update and restore final number
+        currentNum=Encoder.updateNum(currentNum)
+        
+        for i in range(int(binaryinput[i])):
+            final+=str(currentNum)
+        
+        # clean and return final
         final=final[1:]
-    return final
-encoded=encode(binput) 
-print(encoded)
-# take in input and decode it 
-def decode(binaryinput):
-    final=""
-    currentNum=int(binaryinput[0])
-    for i in range(len(binaryinput)):
-        # need to also check if next char is / without overflow
-        if not (i+1)==len(binaryinput):
-            if binaryinput[i]!="/" and binaryinput[i-1]!="/" and binaryinput[i+1]!="/":
-                # add that amount of the currentNum to final
-                for i in range(int(binaryinput[i])):
-                    final+=str(currentNum)
-            elif binaryinput[i]=="/":
-                # parse longer numbers
-                amount=binaryinput[i-1]+binaryinput[i+1]
-                for i in range(int(amount)):
-                    final+=str(currentNum)
-            else:
-                continue
-        # update current number
-        currentNum=updateNum(currentNum)
-    # update and restore final number
-    currentNum=updateNum(currentNum)
-    for i in range(int(binaryinput[i])):
-        final+=str(currentNum)
-    # clean and return final
-    final=final[1:]
-    return final
-decoded=decode(encoded)
-print(decoded)
+        return final
